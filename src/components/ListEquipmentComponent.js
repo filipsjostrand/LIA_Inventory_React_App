914d7f13b04539fffbd89658ad6417e290c235df
filipsjostrand <92868216+filipsjostrand@users.noreply.github.com>
2022-05-16 16:57:17 +0200

    Add add equipment form AddEquipmentComponent

--- src/components/ListEquipmentComponent.js
+++ src/components/ListEquipmentComponent.js
@@ -7,79 +7,78 @@ import EquipmentService from '../services/EquipmentService';
 
 
 
-    /* FrontEnd-formulär (i div)
-    
-    <form>
-        <input placeholder="Add equipment" />
-        <button onClick="onSubmit()">Spara</button>
-    </form>
-    */
+/* FrontEnd-formulär (i div)
+ 
+<form>
+    <input placeholder="Add equipment" />
+    <button onClick="onSubmit()">Spara</button>
+</form>
+*/
 
-    export class ListEquipmentComponent extends React.Component {
+export class ListEquipmentComponent extends React.Component {
 
-        constructor() {
-            super()
-            this.state = {
-                equipment: [
-                    { id: 0, unique_id_serial: '111112', model_name: 'iPhone 13', date_of_purchase: '2022-01-10' },
-                    { id: 1, unique_id_serial: '888AAA', model_name: 'MacBook Pro (14-inch, 2021)', date_of_purchase: '2022-01-10' },
-                ]
-            }
-
-        }
-
-        componentDidMount() {
-            EquipmentService.getEquipment().then((response) => {
-                console.log(response)
-                this.setState({ equipment: response.data })
-            });
-        }
-
-        render() {
-            return (
-                <div>
-                    <h2 className="text-center"> Equipment list </h2>
-                    <div className="row">
-                        <button className="btn btn-primary" onClick={this.addEquipment}> Add Equipment</button>
-
-                            <Link to="/add-equipment">
-                                <button className="btn btn-primary">Add Equipment 2</button>
-                            </Link>
-                    </div>
-                    <table className="table table-striped">
-                        <thead>
-                            <tr>
-                                <td> Id</td>
-                                <td> Unique Id Serial</td>
-                                <td> Model Name</td>
-                                <td> Date of Purchase</td>
-
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                this.state.equipment.map(
-                                    equipment =>
-                                        <tr key={equipment.id}>
-                                            <td> {equipment.id}</td>
-                                            <td> {equipment.unique_id_serial}</td>
-                                            <td> {equipment.model_name}</td>
-                                            <td> {equipment.date_of_purchase}</td>
-
-                                        </tr>
-                                )
-
-                            }
-
-                        </tbody>
-                    </table>
-
-                </div>
-            )
-
+    constructor() {
+        super()
+        this.state = {
+            equipment: [
+                { id: 0, unique_id_serial: '111112', model_name: 'iPhone 13', date_of_purchase: '2022-01-10' },
+                { id: 1, unique_id_serial: '888AAA', model_name: 'MacBook Pro (14-inch, 2021)', date_of_purchase: '2022-01-10' },
+            ]
         }
 
     }
+
+    componentDidMount() {
+        EquipmentService.getEquipment().then((response) => {
+            console.log(response)
+            this.setState({ equipment: response.data })
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h2 className="text-center"> Equipment list </h2>
+                <div className="row">
+
+                    <Link to="/add-equipment">
+                        <button className="btn btn-primary">Add Equipment</button>
+                    </Link>
+                </div>
+                <table className="table table-striped">
+                    <thead>
+                        <tr>
+                            <td> Id</td>
+                            <td> Unique Id Serial</td>
+                            <td> Model Name</td>
+                            <td> Date of Purchase</td>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            this.state.equipment.map(
+                                equipment =>
+                                    <tr key={equipment.id}>
+                                        <td> {equipment.id}</td>
+                                        <td> {equipment.unique_id_serial}</td>
+                                        <td> {equipment.model_name}</td>
+                                        <td> {equipment.date_of_purchase}</td>
+
+                                    </tr>
+                            )
+
+                        }
+
+                    </tbody>
+                </table>
+
+            </div>
+        )
+
+    }
+
+}
 
 
 export default ListEquipmentComponent
